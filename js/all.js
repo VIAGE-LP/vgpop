ad39785bc976c0d83c0386577aacb60d8a441d8f
Pickachu Chen <pickachu1006@gmail.com>
2022-04-26 15:36:59 +0800

    20220426_nav link

--- js/all.js
+++ js/all.js
@@ -6,19 +6,24 @@ $('.navbar-nav>li>a').on('click', function(){
     $('.navbar-light .navbar-toggler ').attr("aria-expanded","false");
 });
 //navbar menu動態
-// $('.navbar-nav>li>a[href="#"], .logo-router>h1>a').on('click', function () {
-//     $('html,body').animate({
-//         scrollTop: 0,
-//      }, 500);
-// });
-// $('.navbar-nav>li>a[href="#support"]').on('click', function () {
-//     $('html,body').animate({
-//         scrollTop: $('#support').offset().top,
-//      }, 500);
-// });
-// $('.navbar-nav>li>a[href="#introduction"]').on('click', function () {
-//     $('html,body').animate({
-//         scrollTop: $('#introduction').offset().top,
-//      }, 500);
-// });
+$('.navbar-nav>li>a[href="#"], .logo-router>h1>a').on('click', function () {
+    $('html,body').animate({
+        scrollTop: 0,
+     }, 500);
+});
+$('.navbar-nav>li>a[href="#shopsale"]').on('click', function () {
+    $('html,body').animate({
+        scrollTop: $('#shopsale').offset().top-70,
+     }, 500);
+});
+$('.navbar-nav>li>a[href="#reserve"]').on('click', function () {
+    $('html,body').animate({
+        scrollTop: $('#reserve').offset().top-70,
+     }, 500);
+});
+$('.navbar-nav>li>a[href="#traffic"]').on('click', function () {
+    $('html,body').animate({
+        scrollTop: $('#traffic').offset().top-70,
+     }, 500);
+});
 });
